149fb6a78efa115310a077578858a271345a8c83
Marta Sędek <pawel.seedek@gmail.com>
2022-07-18 21:46:56 +0200

    init project

---
+++ src/main/java/com/project/riva/RivaApplication.java
@@ -0,0 +1,13 @@
+package com.project.riva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/riva/RivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.riva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
