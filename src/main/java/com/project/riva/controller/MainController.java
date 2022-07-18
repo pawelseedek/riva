cbb57abc785a17e1dde691b838360897a029af10
martaseedek <marta.seedek@gmail.com>
2022-07-18 22:47:59 +0200

    Added main conteller.

---
+++ src/main/java/com/project/riva/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.project.riva.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Hello";
+    }
+}
